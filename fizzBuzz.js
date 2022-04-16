a3a35b5b8aa8520288ff51bb48dc29dae0b5dad3
Adrie <adriebuendia@icloud.com>
2022-04-15 23:20:26 -0400

    first try

---
+++ fizzBuzz.js
@@ -0,0 +1,14 @@
+// Write your solution below this line:
+
+for (let i = 1; i <= 50; i++); {
+    if (i % 15 == 0)
+    console.log('Fizzbuzz');
+    else if (i % 5 == 0) 
+    console.log('Buzz');
+    else if (i % 3 == 0) 
+    console.log('Fizz');
+    else 
+    console.log(i);
+    
+}
+
